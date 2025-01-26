c2ba7d8b6f1fff91bba29b6e7202bad46fb12f7b
Dalzsfn <dalz20033@gmail.com>
2025-01-26 11:24:28 -0500

    RolDTO

--- src/DataAccess/DTO/RolDTO.java
+++ src/DataAccess/DTO/RolDTO.java
@@ -1,48 +1,33 @@
 package DataAccess.DTO;
 
-public class CATALOGODTO {
-    private Integer id_catalogo;
-    private Integer id_tipo;
-    private Integer id_padre;
+public class RolDTO {
+    
+    private Integer id_rol;
     private String nombre;
-    private String estado; 
+    private String estado;
     private String fecha_creacion;
     private String fecha_modificacion;
-    
-    public CATALOGODTO(){}
 
-    public CATALOGODTO(Integer id_tipo, String nombre) {
-        this.id_tipo = id_tipo;
+    public RolDTO(){}
+
+    public RolDTO(Integer id_rol, String nombre) {
+        this.id_rol = id_rol;
         this.nombre = nombre;
     }
-
-    public CATALOGODTO(Integer id_catalogo, Integer id_tipo, Integer id_padre, String nombre, String estado,
-            String fecha_creacion, String fecha_modificacion) {
-        this.id_catalogo = id_catalogo;
-        this.id_tipo = id_tipo;
-        this.id_padre = id_padre;
+    public RolDTO(Integer id_rol, String nombre, String estado, String fecha_creacion, String fecha_modificacion) {
+        this.id_rol = id_rol;
         this.nombre = nombre;
         this.estado = estado;
         this.fecha_creacion = fecha_creacion;
         this.fecha_modificacion = fecha_modificacion;
     }
-    public Integer getId_catalogo() {
-        return id_catalogo;
+
+    
+    public Integer getId_rol() {
+        return id_rol;
     }
-    public void setId_catalogo(Integer id_catalogo) {
-        this.id_catalogo = id_catalogo;
-    }
-    public Integer getId_tipo() {
-        return id_tipo;
-    }
-    public void setId_tipo(Integer id_tipo) {
-        this.id_tipo = id_tipo;
-    }
-    public Integer getId_padre() {
-        return id_padre;
-    }
-    public void setId_padre(Integer id_padre) {
-        this.id_padre = id_padre;
+    public void setId_rol(Integer id_rol) {
+        this.id_rol = id_rol;
     }
     public String getNombre() {
         return nombre;
@@ -71,13 +56,10 @@ public class CATALOGODTO {
 
     public String toString() {
         return getClass().getName()
-        + "\n Id_catalogo:  " + getId_catalogo()
-        + "\n Id_Tipo:  " + getId_tipo()
-        + "\n Id_padre:  " + getId_padre()
+        + "\n Id_rol:  " + getId_rol()
         + "\n Nombre: " + getNombre()
         + "\n Estado: " + getEstado()
         + "\n FechaCreacion:  " +  getFecha_creacion()
         + "\n Fecha_modificacion=" + getFecha_modificacion();
     }
 }
-
