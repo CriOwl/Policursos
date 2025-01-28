987ccf75ff959fc934c4d3b91fe1766160a85e0b
Joe-575 <medinajoel814@gmail.com>
2025-01-28 11:48:39 -0500

    Cambios en DataHelper No significativos

--- src/DataAccess/SQLiteDataHelper.java
+++ src/DataAccess/SQLiteDataHelper.java
@@ -14,7 +14,7 @@ public abstract class SQLiteDataHelper {
             if(conn == null)
                 conn = DriverManager.getConnection(DBPathConnection);
         } catch (SQLException e) {
-             throw e; //new Exception(e,"SQLiteDataHelper","Fallo la coneccion a la base de datos");
+            System.out.println(e);;
         } 
         return conn;
     }
@@ -24,7 +24,7 @@ public abstract class SQLiteDataHelper {
             if (conn != null)
                 conn.close();
         } catch (Exception e) {
-            throw e;//new Exception(e,"SQLiteDataHelper", "Fallo la conección con la base de datos");
+            System.out.println(e);
         }
     }
 }
