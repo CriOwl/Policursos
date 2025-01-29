a61936c9956d5ec379354df91db4cf7ff7494083
MicMiuw <karinacardenasdiaz2005@hotmail.com>
2025-01-28 23:52:40 -0500

    Update Estudiante.java

--- src/BusinessLogic/Entities/Estudiante.java
+++ src/BusinessLogic/Entities/Estudiante.java
@@ -1,10 +1,13 @@
 package BusinessLogic.Entities;
 
 import BusinessLogic.BLFactory;
-import DataAccess.DAO.CursoDAO;
-import DataAccess.DTO.CursoDTO;
+import DataAccess.DAO.Estudiante_cursoDAO;
+import DataAccess.DTO.Estudiante_cursoDTO;
 
-public class Estudiante extends Persona{
+import java.util.ArrayList;
+import java.util.List;
+
+public class Estudiante extends Persona {
 
     private double nota;
 
@@ -21,9 +24,37 @@ public class Estudiante extends Persona{
         this.nota = nota;
     }
 
-    private static void getNotaDB() throws Exception{
-        BLFactory<Estudiante_cursoDTO> oEstudiante_cursoBL = new BLFactory<>(Estudiante_cursoDAO::new);
-        Estudiante_curso.nota = oEstudiante_curso.getBy(1).getNota(); 
+
+    public static Estudiante getEstudianteById(int id) throws Exception {
+        BLFactory<Estudiante_cursoDTO> estudianteFactory = new BLFactory<>(Estudiante_cursoDAO::new);
+        Estudiante_cursoDTO estudianteDTO = estudianteFactory.getBy(id);
+        return new Estudiante(estudianteDTO.getId_estudiante().toString(), "", estudianteDTO.getNota());
     }
 
+    public static List<Estudiante> getAllEstudiantes() throws Exception {
+        BLFactory<Estudiante_cursoDTO> estudianteFactory = new BLFactory<>(Estudiante_cursoDAO::new);
+        List<Estudiante_cursoDTO> estudianteDTOList = estudianteFactory.getAll();
+        List<Estudiante> estudiantes = new ArrayList<>();
+        for (Estudiante_cursoDTO dto : estudianteDTOList) {
+            estudiantes.add(new Estudiante(dto.getId_estudiante().toString(), "", dto.getNota()));
+        }
+        return estudiantes;
+    }
+
+    public static boolean addEstudiante(Estudiante estudiante) throws Exception {
+        BLFactory<Estudiante_cursoDTO> estudianteFactory = new BLFactory<>(Estudiante_cursoDAO::new);
+        Estudiante_cursoDTO estudianteDTO = new Estudiante_cursoDTO(estudiante.getCedula(), estudiante.getCurso(), estudiante.getNota(), "A", null, null);
+        return estudianteFactory.add(estudianteDTO);
+    }
+
+    public static boolean updateEstudiante(Estudiante estudiante) throws Exception {
+        BLFactory<Estudiante_cursoDTO> estudianteFactory = new BLFactory<>(Estudiante_cursoDAO::new);
+        Estudiante_cursoDTO estudianteDTO = new Estudiante_cursoDTO(estudiante.getId(), estudiante.getCedula(), estudiante.getCurso(), estudiante.getNota(), "A", null, null);
+        return estudianteFactory.upd(estudianteDTO);
+    }
+
+    public static boolean deleteEstudiante(int id) throws Exception {
+        BLFactory<Estudiante_cursoDTO> estudianteFactory = new BLFactory<>(Estudiante_cursoDAO::new);
+        return estudianteFactory.del(id);
+    }
 }
